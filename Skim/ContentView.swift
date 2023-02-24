38f9037060dfe37a547af4979080c82224c92dd1
Pedro Henriques <33936309+pedromnh@users.noreply.github.com>
2023-02-24 10:29:02 +0000

    Read sentence word per word with incomplete wpm timer.

--- Skim/ContentView.swift
+++ Skim/ContentView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var phrase = "As @Paul and @John have suggested, you can use a Timer to do this. However, if the code does need to be asynchronous for some reason, you can reimplement the loop asynchronously by making it recursive:"
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(
+                action: {readString(sentence: phrase, wpm: 0.1)},
+                label: {Text("Click me!")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(15)
+            })
+        }
+    }
+    
+}
+
+
+func readString(sentence: String, wpm: Double) {
+    let words = sentence.components(separatedBy: " ")
+    
+    var i = 0
+    _ = Timer.scheduledTimer(withTimeInterval: wpm, repeats: true) { t in
+        print(words[i]);
+        i += 1;
+        if i >= words.count {
+            t.invalidate();
+        }
     }
 }
+
+func calculateWpmSpeed(wpm: Double) -> Double {
+    let wordsPerSecond = wpm / 60
+    return 1 / wordsPerSecond
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
